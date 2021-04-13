51d989420a2e9ce044bd228bdbc754fe7dbf48b0
DennisFeldbusch <d.feldbusch@web.de>
2021-04-13 14:28:54 +0200

    Added empty script to install all requirements for pentesting

---
+++ scripts/installRequirements.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Author: S0teri
+# Date: 13.04.2021
+# Purpose: This script should be executed to install all required tools and other stuff to have a solid base to pentest/hunt bugs
+
+
