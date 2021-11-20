56b89d3874aab229b0d1bf4b68a56e285bc40c56
DennisFeldbusch <d.feldbusch@web.de>
2021-11-20 14:35:11 +0100

    continued

--- scripts/installRequirements.sh
+++ scripts/installRequirements.sh
@@ -2,6 +2,11 @@
 
 # Author: S0teri
 # Date: 13.04.2021
-# Purpose: This script should be executed to install all required tools and other stuff to have a solid base to pentest/hunt bugs
+# Purpose: This script should be executed to install all required tools and other stuff to have a solid base for pentesting/bug hunting
+
+# Installing nmap
+sudo pacman -S nmap
+
+#Installing 
 
 
